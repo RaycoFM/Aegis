5c7e9bd219fbcd36b6a9348a63ccdcf327f5ed65
RaycoFM <rayco910409@gmail.com>
2021-11-12 14:37:10 +0100

    b

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,17 @@
-module.exports = {
+/*module.exports = {
     database: {
         host: 'PMYSQL155.dns-servicio.com',
         user: 'aegis',
         password: 'Swingter*123',
         database: '9042491_prueba'
+    }
+};*/
+
+module.exports = {
+    database: {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'test'
     }
 };
