904ac5b7243c97ce44b02a1b7aa604778193014f
RaycoFM <rayco910409@gmail.com>
2021-11-17 13:30:07 +0100

    d

--- src/keys.js
+++ src/keys.js
@@ -1,11 +1,20 @@
 module.exports = {
     database: {
+        host: 'localhost',
+        user: 'root',
+        password: 'Swingter*12',
+        database: 'aegis'
+    }
+};
+
+/*module.exports = {
+    database: {
         host: 'PMYSQL155.dns-servicio.com',
         user: 'aegis',
         password: 'Swingter*123',
         database: '9042491_prueba'
     }
-};
+};*/
 
 /*module.exports = {
     database: {
