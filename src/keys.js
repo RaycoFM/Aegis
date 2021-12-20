ba30fb32cdebc9590845b1503e8e8f07281780aa
RaycoFM <rayco910409@gmail.com>
2021-12-20 22:18:07 +0100

    e

--- src/keys.js
+++ src/keys.js
@@ -1,11 +1,11 @@
-/*module.exports = {
+module.exports = {
     database: {
         host: 'localhost',
         user: 'root',
         password: 'Swingter*12',
         database: 'aegis'
     }
-};*/
+};
 
 /*module.exports = {
     database: {
@@ -15,7 +15,7 @@
         database: '9042491_prueba'
     }
 };*/
-
+/*
 module.exports = {
     database: {
         host: 'localhost',
@@ -23,4 +23,4 @@ module.exports = {
         password: '',
         database: 'test'
     }
-};
+};*/
